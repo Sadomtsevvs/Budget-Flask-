f296664817bf11baaee4d067bd2d06ef50161ae4
Sadomtsevvs <67393853+Sadomtsevvs@users.noreply.github.com>
2022-04-23 21:27:38 +0300

    Forms

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "mydatabase.db"))
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+db = SQLAlchemy(app)
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(50), nullable = False)
+    expensename = db.Column(db.String(50), nullable = False)
+    amount = db.Column(db.Integer, nullable = False)
+    category = db.Column(db.String(50), nullable = False)
 
 
 @app.route('/')
 def add():
     return render_template('add.html')
+
+@app.route('/addexpense', methods=['POST'])
+def addexpense():
+    date = request.form['date']
+    expensename = request.form['expensename']
+    amount = request.form['amount']
+    category = request.form['category']
+    print(date, expensename, amount, category)
+
+    return redirect('/')
 
 
 if __name__ == '__main__':
